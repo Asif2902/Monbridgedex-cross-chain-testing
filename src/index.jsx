b5dedbd9091c2583f466e79a6693068918729fa7
Asif <107271220+Asif2902@users.noreply.github.com>
2025-07-01 20:24:15 +0600

    update src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { config } from '../src/wagmi.js';
+import App from './App';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
